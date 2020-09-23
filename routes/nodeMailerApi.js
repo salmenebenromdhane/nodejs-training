9e88cd6cc8c800eb54cfcf864c2127e3eeff7746
salmenebenromdhane <47206492+salmenebenromdhane@users.noreply.github.com>
2020-09-23 14:52:30 +0100

    refactor nodemailer with html & ejs

--- routes/nodeMailerApi.js
+++ routes/nodeMailerApi.js
@@ -3,11 +3,12 @@
 const express=require('express')
 const router=express.Router()
 const nodemailer = require("nodemailer");
-
+var ejs = require("ejs");
+var path=require('path');
 require('dotenv').config();
 
 
-// add new todo
+// sending simple mail
 router.post('/add', async (req,res)=>{
 // Step 1
 let transporter = nodemailer.createTransport({
@@ -38,5 +39,44 @@ transporter.sendMail(mailOptions, (err, data) => {
 
 });
 
+//sending mail with html file
+router.post('/sendMailWithHtml', async (req,res)=>{
 
+  // Step 1
+  let transporter = nodemailer.createTransport({
+    
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL || 'your gmail account', // TODO: your gmail account
+        pass: process.env.PASSWORD || 'your gmail password' // TODO: your gmail password
+    }
+  });
+  
+  // Step 2
+ let fileData = {
+  name: 'Salmene.'
+};
+ejs.renderFile(path.resolve('./views/','EmailTemplate.html'), fileData, function (err, data) {
+  if (err) {
+      console.log(err);
+  } else {
+      var mainOptions = {
+          from: '"salmene.benromdhane@esprit.tn',
+          to: "salmene.benromdhane@esprit.tn",
+          subject: 'Hello, world',
+          html: data
+      };
+     // Step 3
+      transporter.sendMail(mainOptions, function (err, info) {
+        if (err) {
+          res.json(err);
+      }
+          res.json("Email sent!")
+      });
+  }
+  
+  });
+  
+  });
+  
 module.exports=router;
